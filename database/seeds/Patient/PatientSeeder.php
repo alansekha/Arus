65ca27ef53591a43468acc29b56b7ff6836796d5
mhmdbhska <73042546+mhmdbhska@users.noreply.github.com>
2020-10-18 14:20:47 +0700

    Added more sample patient

--- database/seeds/Patient/PatientSeeder.php
+++ database/seeds/Patient/PatientSeeder.php
@@ -20,6 +20,16 @@ class PatientSeeder extends Seeder
         $patient_family->date_of_birth = '2002-05-19';
         $patient_family->place_of_birth = "jakarta";
         $patient_family->save();
+        
+        #sample patient user
+        $patient_family = new patient_family_member;
+        $patient_family->user_id = 2;
+        $patient_family->name = "Bank Jago";
+        $patient_family->nik = 214412444;
+        $patient_family->gender = "male";
+        $patient_family->date_of_birth = '2002-03-01';
+        $patient_family->place_of_birth = "Surabaya";
+        $patient_family->save();
 
     }
 }
